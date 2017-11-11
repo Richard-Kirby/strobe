c7523fd4fe9d0f8c6e32974aa24d95569966f91c
Richard-Kirby <richard.james.kirby@gmail.com>
2017-11-11 16:21:15 +0000

    Add files via upload

---
+++ display/config.py
@@ -0,0 +1,18 @@
+# Standard modules
+import queue
+
+# Specialised modules
+import gaugette.gpio
+import pigpio
+
+# Queue to send Display information.
+DisplayQ = queue.Queue()
+
+# Queue to send LED Control information.
+LEDQ = queue.Queue()
+
+# Set up GPIO to be used by various modules.
+gpio = gaugette.gpio.GPIO()
+
+pi= pigpio.pi()
+
